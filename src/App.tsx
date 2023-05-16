90ee65428765a3bd384c377b4f7546b0bbee0b34
Yohann DC <YohannDCz@gmail.com>
2023-05-16 18:08:04 +0200

    Update splitscreen en responsive tablet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Header } from './components/header'
 import Hero from './components/hero'
-import { SplitScreens } from './components/splitscreens'
+import { SplitScreens } from './components/splitscreens-home'
 import Tagline from './components/tagline'
 import About from './components/about'
 
