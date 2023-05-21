4b734bdf742eda0250e19517c4b96b5ede87a3f9
Yohann DC <YohannDCz@gmail.com>
2023-05-21 15:35:35 +0200

    Update redirects

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/stack" element={<Stack />} />
           <Route path="/contact" element={<Contact />} />
