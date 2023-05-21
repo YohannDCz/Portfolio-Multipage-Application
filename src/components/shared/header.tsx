64ca8caa1479ab8eb2b745996627ced913d480a8
Yohann DC <YohannDCz@gmail.com>
2023-05-21 15:43:08 +0200

    Update Links

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -32,10 +32,10 @@ export function Header() {
         <img onClick={closeDrawer} className="cross" src="/icons/icon_cross.png" alt="The close icon." />
         <img className="logotype" src="/logos/logotype.png" alt="The logotype of the page." />
         <div className="sections">
-          <a href="/"><p className={location.pathname === '/' ? 'purple' : ''}>HOME</p></a>
-          <a href="/projects"><p className={location.pathname === '/projects' ? 'purple' : ''}>PROJECTS</p></a>
-          <a href="/stack"><p className={location.pathname === '/stack' ? 'purple' : ''}>STACK</p></a>
-          <a href="/contact"><p className={location.pathname === '/contact' ? 'purple' : ''}>CONTACT</p></a>
+          <Link to="/"><p className={location.pathname === '/' ? 'purple' : ''}>HOME</p></Link>
+          <Link to="/projects"><p className={location.pathname === '/projects' ? 'purple' : ''}>PROJECTS</p></Link>
+          <Link to="/stack"><p className={location.pathname === '/stack' ? 'purple' : ''}>STACK</p></Link>
+          <Link to="/contact"><p className={location.pathname === '/contact' ? 'purple' : ''}>CONTACT</p></Link>
         </div>
       </div>
       <div className="filter"></div>
