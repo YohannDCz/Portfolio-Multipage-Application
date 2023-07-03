211b5202beb060f081d2542ef2653ec47483697a
Yohann DC <YohannDCz@gmail.com>
2023-07-03 19:31:25 +0200

    Update form.tsx

--- src/components/contact/form.tsx
+++ src/components/contact/form.tsx
@@ -1,59 +1,70 @@
-import { useState } from 'react';
-import sgMail from '@sendgrid/mail';
+import { useState } from "react";
+import sgMail from "@sendgrid/mail";
 // import { SENDGRID_API_KEY } from './sendgridConfig';
 
-interface mailProps {
-  email: string,
-  message: string
-}
+// interface mailProps {
+//   email: string,
+//   message: string
+// }
 
-function sendEmail({email, message}: mailProps) {
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  const msg = {
-    to: 'yohanndcz@gmail.com', // Change to your recipient
-    from: email, // Change to your verified sender
-    subject: 'Nouveau message de ton portfolio',
-    text: message,
-    html: `<strong>${message}</strong>`,
-  };
+// function sendEmail({email, message}: mailProps) {
+//   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+//   const msg = {
+//     to: 'yohanndcz@gmail.com', // Change to your recipient
+//     from: email, // Change to your verified sender
+//     subject: 'Nouveau message de ton portfolio',
+//     text: message,
+//     html: `<strong>${message}</strong>`,
+//   };
 
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent');
-      // Optionally, display a success message or perform other actions
-    })
-    .catch((error) => {
-      console.error(error);
-      // Handle the error, display an error message, or perform other actions
-    });
-}
-
+//   sgMail
+//     .send(msg)
+//     .then(() => {
+//       console.log('Email sent');
+//       // Optionally, display a success message or perform other actions
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       // Handle the error, display an error message, or perform other actions
+//     });
+// }
 
 export function Form() {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+ const [email, setEmail] = useState("");
+ const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    sendEmail({email, message});
-  };
-  return (
-    <div className="form">
-      <div className="text">
-        <h2>Vous êtes recruteur ?</h2>
-        <p>
-          N’hésitez pas à me contacter sur
-          mon adresse mail pour prendre
-          contact en vue d’un entretiens
-          de recrutement
-        </p>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input placeholder="Adresse email" type="text" className="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <textarea placeholder="Message..." name="message" id="" cols={30} rows={10} value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-        <button type="submit">Envoyer</button>
-      </form>
-    </div>
-  )
+ const handleSubmit = async (e: any) => {
+  e.preventDefault();
+  // sendEmail({email, message});
+ };
+ return (
+  <div className="form">
+   <div className="text">
+    <h2>Vous êtes recruteur ?</h2>
+    <p>
+     N’hésitez pas à me contacter sur mon adresse mail pour prendre contact en
+     vue d’un entretiens de recrutement
+    </p>
+   </div>
+   <form onSubmit={handleSubmit}>
+    <input
+     placeholder="Adresse email"
+     type="text"
+     className="email"
+     value={email}
+     onChange={(e) => setEmail(e.target.value)}
+    />
+    <textarea
+     placeholder="Message..."
+     name="message"
+     id=""
+     cols={30}
+     rows={10}
+     value={message}
+     onChange={(e) => setMessage(e.target.value)}
+    ></textarea>
+    <button type="submit">Envoyer</button>
+   </form>
+  </div>
+ );
 }
